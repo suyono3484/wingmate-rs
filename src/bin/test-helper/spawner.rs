4195eadc9aaf12256ef22b4c598bb9fc79d52a9d
Suyono <suyono3484@gmail.com>
2023-12-04 11:04:24 +1100

    test(spawner): added logs

--- src/bin/test-helper/spawner.rs
+++ src/bin/test-helper/spawner.rs
@@ -1,17 +1,39 @@
+#[macro_use] extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
+
 use std::error::Error;
+use std::fs::OpenOptions;
 use std::process::Command;
 use std::env;
+use rand::Rng;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+    let mut rng = rand::thread_rng();
+
+    let log_path = env::var("LOG_PATH")?;
+    let file = OpenOptions::new().append(true).create(true).open(log_path)?;
+    WriteLogger::new(LevelFilter::Debug, Config::default(), file);
     if args.len() > 1 {
         let x: u64 = args[1].parse()?;
-        for _i in 0..x {
-            // println!("spawning {}", _i);
-            Command::new("/usr/local/bin/dummy").arg("10").spawn()?;
+        loop {
+            for _i in 0..x {
+                let sleep_time = rng.gen_range(10..20);
+                info!("starting wmtest-helper-dummy {}", &sleep_time);
+                let child = Command::new("/usr/local/bin/wmtest-helper-dummy").arg(format!("{}", sleep_time)).spawn();
+                if let Err(e) = child {
+                    error!("error spawning child: {e}");
+                }
+            }
+
+            let pause_time = rng.gen_range(10..20);
+            info!("going to sleep for {}", &pause_time);
+            std::thread::sleep(std::time::Duration::from_secs(pause_time));
+            info!("waking up")
         }
     } else {
         return Err(anyhow::anyhow!("invalid arguments").into());
     }
-    Ok(())
 }
